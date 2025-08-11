a44467e1571af48973e43b8ee9aa98e9a9803744
Ali Bagheri <ali.bagheri408@gmail.com>
2025-08-11 15:39:52 +0330

    contact is changhed

--- source/pages/Contact.js
+++ source/pages/Contact.js
@@ -1,8 +1,9 @@
 export default function renderContact() {
   return `
-    <section class="p-6 text-center fade-in mt-16">
+    <section class="p-6 text-center fade-in mt-16 space-y-4">
       <h1 class="text-2xl font-bold mb-4">تماس با ما</h1>
       <p class="text-gray-600 dark:text-gray-300">برای ارتباط با ما، لطفاً ایمیل بزنید به: contact@example.com</p>
+      <p class="text-gray-600 dark:text-gray-300">برای ارتباط با مادر تلگرام به این آی پیام دهید: @destiny</p>
     </section>
   `;
 }
